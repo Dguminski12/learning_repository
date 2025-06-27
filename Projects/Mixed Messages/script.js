2577efb399cab18d55f24b97a7687a1e2398a5f8
Dguminski12 <davidguminski12@gmail.com>
2025-06-27 16:25:54 +0000

    Added 3 arrays with fortune-telling elements

--- Projects/Mixed Messages/script.js
+++ Projects/Mixed Messages/script.js
@@ -1,2 +1,6 @@
-console.log("Hello World!");
+const subjects = ["Your future", "A mysterious stranger", "The stars", "Your inner child"];
+const actions = ["will guide you", "brings good news", "is warning you", "wants to dance"];
+const endings = ["today.", "soon.", "if you let go.", "under the full moon."];
+
+
 
